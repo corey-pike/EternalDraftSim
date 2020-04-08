c9b69a890b6212d50405103c265345aff9e7d200
os9257ah <47129067+os9257ah@users.noreply.github.com>
2020-04-08 15:12:48 -0500

    Header and Header Image

---
+++ headerA4.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Eternal Draft Simulator</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?=$docRoot?>css/style.css" rel="stylesheet" type="text/css" />
+        <script src="<?=$docRoot?>js/site.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id="header" style="background-image: '<?=$docRoot?>images/header.png'">
+                <div id="headerText">
+                    <div id="headerTitle">
+                        Eternal Draft Simulator
+                    </div>
+                    <div id="subtitle">
+                        Home Page
+                    </div>
+                    
+                    <div id="colors">
+                        <table class="loginTable">
+                            <tr>
+                                <td>
+                                    <a href="login.php"><button type="button">Log In</button></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="registration.php"><button type="button">Register</button></a>
+                                </td>
+                            </tr>
+                        </table>
+                        <img id="profilepicture"  src="<?=$docRoot?>images/profile.png" alt="profile picture"/>
+                    </div>
+                </div>
+            </div>
