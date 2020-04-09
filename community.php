2ea8b9988ff4e5920ac1f398ac5ad6d8ff6d8ebb
Corey <jz6142gb@go.minnstate.edu>
2020-04-08 20:59:45 -0500

    forms

--- community.php
+++ community.php
@@ -1,18 +1,19 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>title</title>
-  <meta name="author" content="name">
-  <meta name="description" content="description here">
-  <meta name="keywords" content="keywords,here">
-  <link rel="stylesheet" href="stylesheet.css" type="text/css">
-  </head>
-  <body>
-  
+
+<?php
+    include_once "headerA4.php";
+?>
+<body>
+<h1>Community</h1>
+        <main class="content">
+<?php
+	include "menu.php"
+?>
+        </main>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
   <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js" type="text/javascript"></script>
   </body>
+
+<?php
+    include_once "footerA4.php";
+?>
 </html>
