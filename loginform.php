beb88f434bddddd91a31252a8b72a1f70b6f4e3c
os9257ah <47129067+os9257ah@users.noreply.github.com>
2020-04-15 16:46:14 -0500

    Form edits
    adding names and other attributes, removing blank ids

--- loginform.php
+++ loginform.php
@@ -11,26 +11,28 @@
 	<link rel="stylesheet" href="stylesheet.css" type="text/css">
 	</head>
 	<body id="" >
-		<div id="">
+		<div id="content">
 			<h1>
 				<a>Log In</a>
 			</h1>
 			<small>this will be a popup window</small>
-			<form id="login" class=""  method="post" action="">					
+                        <form id="login" class=""  method="post" action="index.php">					
 				<ul >
-					<li id="" >
-						<label class="" for="">Username </label>
+					<li>
+						<label class="formLabel" for="username">Username </label>
 						<div>
-							<input id="" name="" class="" type="text" maxlength="50" pattern="[a-zA-Z0-9]{1,}#[0-9]{4}" title="Can only use alphanumeric characters and must end with '#' followed by 4 numbers"/> 
+							<input name="username" class="" type="text" maxlength="50" pattern="[a-zA-Z0-9]{1,}#[0-9]{4}" title="Can only use alphanumeric characters and must end with '#' followed by 4 numbers"/> 
 						</div>
 					</li>
-					<li id="" >
-						<label class="" for="">Password </label>
+					<li>
+						<label class="formLabel" for="password">Password </label>
 						<div>
 							<input type="password" id="password" name="password" pattern="^([a-zA-Z0-9@*#]{8,15})$" title="Password must consists of at least 8 characters and not more than 15 characters">
 						</div> 
 					</li>
+                                        <li>
 						<input id="saveForm" class="" type="submit" name="submit" value="Submit" />
+                                        </li>
 				</ul>
 			</form>	
 		</div>
--- profileform.php
+++ profileform.php
@@ -11,31 +11,33 @@
 			</h1>
                     <form id="profile" class=""  method="post" action="profile.php">					
 				<ul >
-					<li id="" >
-						<label class="" for="">Eternal Username </label>
+					<li>
+						<label class="formLabel" for="username">Eternal Username </label>
 						<div>
-							<input id="" name="" class="" type="text" maxlength="50" pattern="[a-zA-Z0-9]{1,}#[0-9]{4}" title="Can only use alphanumeric characters and must end with '#' followed by 4 numbers"/>
+							<input name="username" class="" type="text" maxlength="50" pattern="[a-zA-Z0-9]{1,}#[0-9]{4}" title="Can only use alphanumeric characters and must end with '#' followed by 4 numbers"/>
 						</div>
 					</li>
-					<li id="" >
-						<label class="" for="">Email </label>
+					<li>
+						<label class="" for="email">Email </label>
 						<div>
-							<input id="" name="" class="" type="text" maxlength="50" pattern="^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$" title="Must be a valid email"/>  
+							<input name="email" class="" type="text" maxlength="50" pattern="^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$" title="Must be a valid email"/>  
 						</div>
 					</li>
-					<li id="" >
-						<label class="" for="">Password </label>
+					<li>
+						<label class="" for="password">Password </label>
 						<div>
 							<input type="password" id="password" name="password" pattern="^([a-zA-Z0-9@*#]{8,15})$" title="Password must consists of at least 8 characters and not more than 15 characters">
 						</div> 
 					</li>
-					<li id="" >
-						<label class="" for="">Confirm Password </label>
+					<li>
+						<label class="" for="password2">Confirm Password </label>
 						<div>
-							<input type="password" id="password" name="password" pattern="^([a-zA-Z0-9@*#]{8,15})$" title="Passwords must match"> 
+							<input type="password" id="password2" name="password2" pattern="^([a-zA-Z0-9@*#]{8,15})$" title="Passwords must match"> 
 						</div> 
 					</li>
+                                        <li>
 						<input id="saveForm" class="" type="submit" name="submit" value="Submit" />
+                                        </li>
 				</ul>
 			</form>	
 		</div>
