251578facda42d7731fcae50caf7448a508c1477
os9257ah <47129067+os9257ah@users.noreply.github.com>
2020-04-15 21:05:42 -0500

    Forms
    included header and footer in login and registration form, fixed syntax issues

--- loginform.php
+++ loginform.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>Login</title>
-	<meta name="author" content="name">
-	<meta name="description" content="description here">
-	<meta name="keywords" content="keywords,here">
-	<link rel="stylesheet" href="stylesheet.css" type="text/css">
-	</head>
-	<body id="" >
+<?php
+	include "headerA4.php";
+?>
+<main class="content">
 		<div id="content">
 			<h1>
 				<a>Log In</a>
@@ -38,5 +29,7 @@
 		</div>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"/>
 		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js" type="text/javascript"/>
-	</body>
-</html>
+	</main>
+<?php
+    include "footerA4.php";
+?>
